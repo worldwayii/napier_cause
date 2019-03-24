dbe8e6f714436d7c65ee2e1489d50a520e23917c
Elui Christian <eluichuma@gmail.com>
2019-03-24 15:21:10 +0000

    Initial check-in of module Napier_Cause

---
+++ wwwroot/js/scroll.js
@@ -0,0 +1,39 @@
+﻿
+      
+        $(function () {
+            // Slideshow 4
+            $("#slider4").responsiveSlides({
+                auto: true,
+                pager: true,
+                nav: true,
+                speed: 500,
+                namespace: "callbacks",
+                before: function () {
+                    $('.events').append("<li>before event fired.</li>");
+                },
+                after: function () {
+                    $('.events').append("<li>after event fired.</li>");
+                }
+            });
+
+        });
+
+        
+        $(function () {
+            // Slideshow 4
+            $("#slider3").responsiveSlides({
+                auto: true,
+                pager: false,
+                nav: false,
+                speed: 500,
+                namespace: "callbacks",
+                before: function () {
+                    $('.events').append("<li>before event fired.</li>");
+                },
+                after: function () {
+                    $('.events').append("<li>after event fired.</li>");
+                }
+            });
+
+        });
+
---
+++ wwwroot/js/width.js
@@ -0,0 +1,11 @@
+﻿
+/* init Jarallax */
+$('.jarallax').jarallax({
+    speed: 0.5,
+    imgWidth: 1366,
+    imgHeight: 768
+})
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
